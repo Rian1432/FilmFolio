9a6554b56b350438789df8c3aa58aa19f2b30c8c
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2024-04-25 19:43:35 -0300

    definir paleta de cores da aplicacao

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#111111",
+          "secondary": "#18181B",
+          "accent": "#DDAC00",
+          "neutral": "#517C8C",
+          "base-100": "#F7FBEF",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
 
