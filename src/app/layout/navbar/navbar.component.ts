603bbca015a9e0bdc32d8927c21db6fb60444244
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2024-06-17 23:58:42 -0300

    feat: Ajustar roteamento entre as paginas

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
