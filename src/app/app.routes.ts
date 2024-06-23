2db3a062aac0b18d95a9d5abc811f87149eef18c
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2024-06-23 01:38:37 -0300

    refactor: Remover componentes inutilizados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import {MoviesComponent} from "./pages/movies/movies.component";
 import {ShowsComponent} from "./pages/shows/shows.component";
+import {MoviesComponent} from "./pages/movies/movies.component";
 import {MyMoviesComponent} from "./pages/my-movies/my-movies.component";
-import {LoginComponent} from "./pages/login/login.component";
-import {RegisterComponent} from "./pages/register/register.component";
 import {MovieDetailsComponent} from "./pages/movie-details/movie-details.component";
 import {permissionGuard} from "./guards/permission.guard";
 
@@ -16,7 +14,5 @@ export const routes: Routes = [
   },
   {path: 'series', component: ShowsComponent},
   {path: 'meus-filmes', component: MyMoviesComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
   { path: '',   redirectTo: '/filmes', pathMatch: 'full' }
 ];
