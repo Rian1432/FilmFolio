9432f8a4d6bf8ce5b3093243ec5bd40fd02bd5ca
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2024-06-23 13:50:30 -0300

    fix: utilizar https na url da api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,6 +4,6 @@ interface Environment {
 }
 
 export const environment:Environment = {
-  baseUrl: 'http://www.omdbapi.com/?apikey=12119c8d',
+  baseUrl: 'https://www.omdbapi.com/?apikey=12119c8d',
   mockApi: 'https://667319676ca902ae11b31e58.mockapi.io/api/v1/my-movies'
 };
