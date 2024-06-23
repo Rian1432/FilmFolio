10c18c693f47cc9e44279aad2b4bf0f312545197
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2024-06-23 01:40:50 -0300

    refactor: Remover arquivos de estilo inutilizados

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,7 +11,6 @@ import {CommonModule} from "@angular/common";
     RouterLinkActive
   ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
